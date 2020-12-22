ea393b37062aeb593c28a9bc032c85351c596574
Bonfire <baranb@knights.ucf.edu>
2020-12-21 23:45:08 -0500

    Add meta file, Update script meta with update/download URL

---
+++ bptf-bot-utilities.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Backpack.tf - Bot Utilities
+// @namespace    https://github.com/Bonfire
+// @version      1.0
+// @description  A script to provide various TF2Autobot utilities on backpack.tf
+// @author       Bon
+// @downloadURL  https://github.com/Bonfire/bptf-bot-utilities/raw/master/bptf-bot-utilities.user.js
+// @updateURL    https://github.com/Bonfire/bptf-bot-utilities/raw/master/bptf-bot-utilities.meta.js
+// @include      /^https?:\/\/backpack\.tf\/.*
+// @grant        GM_setClipboard
+// @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @run-at       document-end
+// ==/UserScript==
